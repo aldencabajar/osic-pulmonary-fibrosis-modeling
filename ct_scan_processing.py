0de553d5353f9f703724b3f5c587dc026118d666
aldencabajar <aldenacabajar@gmail.com>
2020-09-06 23:25:45 +0800

    initial commit

---
+++ ct_scan_processing.py
@@ -0,0 +1,199 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+import tensorflow_probability as tfp
+import pydicom
+import os
+import re
+from tqdm import tqdm
+import seaborn as sns
+from PIL import Image
+from IPython.display import Image as show_gif
+from scipy import ndimage
+tfd = tfp.distributions
+tfb = tfp. bijectors
+from skimage import morphology
+from skimage import measure
+from sklearn import cluster
+from scipy.stats import skew, kurtosis
+
+
+def create_image(scans):
+    image = np.stack([s.pixel_array for s in scans])
+    image = image.astype(np.int16)
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+
+    # Convert to Hounsfield units (HU)
+    intercept = scans[0].RescaleIntercept
+    slope = scans[0].RescaleSlope
+
+    if slope != 1:
+        image = slope * image.astype(np.float64)
+        image = image.astype(np.int16)
+
+    image += np.int16(intercept)
+
+    return np.array(image, dtype=np.int16)
+
+
+def masking(img):
+    row_size = img.shape[0]
+    col_size = img.shape[1]
+
+    # standardize values
+    mean = np.mean(img)
+    std = np.std(img)
+    img = img - mean
+    img = img/std
+
+    max = np.max(img)
+    min = np.min(img)
+    middle = img[int(col_size/5):int(col_size/5*4),int(row_size/5):int(row_size/5*4)]
+    mean = np.mean(middle)
+    img[img==max] = mean
+    img[img==min] = mean
+
+    # differentiate between tissues and air/empty space by kmeans
+    kmeans = cluster.KMeans(2)
+    kmeans.fit(img)
+    centers = sorted(kmeans.cluster_centers_.flatten())
+    #apply threshold
+    threshold = np.mean(centers)
+    thres_img = np.where(img < threshold, 1., 0.)
+
+    # apply erosion and dilation to differentiate the lungs further
+    thres_image_erosion = morphology.erosion(thres_img, np.ones([3,3]))
+    thres_image_dilation = morphology.dilation(thres_image_erosion, np.ones([8,8]))
+
+    # obtain labels for boundaries
+    labels = measure.label(thres_image_dilation)
+    label_vals = np.unique(labels)
+    regions = measure.regionprops(labels)
+
+    # determine if bounds are good (i.e. they cover the entirety of the lungs)
+    good_labels = []
+    for prop in regions:
+        B = prop.bbox
+        if B[2]-B[0]<row_size/10*9 and B[3]-B[1]<col_size/10*9 and B[0]>row_size/5 and B[2]<col_size/5*4:
+            good_labels.append(prop.label)
+    # initialize mask with zeros (which means that this will be black)
+    mask = np.zeros([row_size,col_size],dtype=np.int8)
+
+    # add the good labels to the mask
+    mask_size = 0
+    for N in good_labels:
+        mask = mask + np.where(labels==N,1,0)
+        mask_size += np.sum(labels == N)
+
+    mask = morphology.dilation(mask,np.ones([10, 10]))
+    final_img = mask * img
+
+    # apply mask to image
+    return final_img, mask_size
+
+
+
+def image_preprocess(pid_dir, sample = None, img_size = [512, 512]):
+    """
+    overall preprocessing step for volumetric images.
+    pid_dir = directory for corresponding patient id
+    sample = number of images to use for volume, will try to sample 2d images at equidistant steps
+    """
+    pid_dir = pid_dir.numpy().decode("utf-8")
+    print("processing", pid_dir)
+    filenames = os.listdir(pid_dir)
+    slices = [pydicom.dcmread(pid_dir + "/" + i) for i in filenames]
+    slices = [s for s in slices if 'SliceLocation' in s]
+    slices.sort(key = lambda x: int(x.InstanceNumber))
+
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] -
+                                slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation -
+                                slices[1].SliceLocation)
+
+    if sample is None:
+        sample = len(slices)
+    slices = [slices[j] for i,j in enumerate(range(0, len(slices), len(slices)//sample)) if i < sample]
+    pixel_spacing = np.unique(list(slices[0]['PixelSpacing']))
+
+    img = create_image(slices)
+    #resize image
+    img = tf.transpose(img, perm = [1,2,0])
+    img = tf.image.resize(img, img_size).numpy()
+
+    #return new_img
+    lung_pixel_size = []
+    masked_imgs = []
+    for i in range(img.shape[2]):
+        masked, lp_size = masking(img[:, :, i])
+        lung_pixel_size.append(lp_size)
+        masked_imgs.append(masked)
+
+    masked_imgs = tf.transpose(np.stack(masked_imgs), perm = [1,2,0])
+
+    return tf.cast(masked_imgs, tf.float32), slice_thickness[None,...], pixel_spacing, np.array(lung_pixel_size)
+
+def get_volume(img, slice_thickness, pixel_spacing, lung_pixel_size):
+    img = img.numpy()
+    s = slice_thickness.numpy()
+    p = pixel_spacing.numpy()
+
+    # determine the maximum lung pixel size
+    max_lung_px = tf.math.reduce_max(lung_pixel_size)
+    idx = tf.reshape(tf.where(lung_pixel_size == max_lung_px), [-1])
+    volume = tf.cast(max_lung_px,tf.float32) * s * p
+
+
+    return img[:, :, idx], volume
+
+def calculate_statistics(img):
+    img = img.numpy()
+    non_zero_idx = np.where(img > 0)
+    non_zero_pixels = img[non_zero_idx[0], non_zero_idx[1]]
+
+    # calculate mean
+    mean = np.mean(non_zero_pixels)
+    # calculate variance
+    var = np.var(non_zero_pixels)
+    #calculate skew
+    skew_ = skew(non_zero_pixels)
+    # kurtosis
+    kurt = kurtosis(non_zero_pixels)
+
+    return np.array([mean, var, skew_[0], kurt[0]])
+
+if __name__ == "__main__":
+    # gather ids and folders for train dataset
+    patient_dcm_dict = {}
+    root_dir = "/kaggle/input/osic-pulmonary-fibrosis-progression/train/"
+    for dirname, _, filenames in os.walk(root_dir):
+        if 'ID' in dirname:
+            dirname_ = dirname.replace(root_dir, "")
+            patient_dcm_dict[dirname_] = filenames
+
+    train_pids = [root_dir + pid + "/" for pid in list(patient_dcm_dict.keys())]
+    dataset = tf.data.Dataset.from_tensor_slices(train_pids)
+    dataset = dataset.map(lambda x:
+                          tf.py_function(image_preprocess, [x],
+                                       [tf.float32, tf.float32, tf.float32, tf.float32]))
+
+    ## calculates the lung volume, and get the pixel array of the max lung volume
+    lung_vol_pixel = dataset.map(lambda x, s, p, lp: tf.py_function(get_volume, [x, s, p, lp],
+                                                                    [tf.float32, tf.float32]))
+    ## calculate needed statistics from masked pixel array
+    lung_img_statistics = lung_vol_pixel.map(lambda img, _:
+                                             tf.py_function(
+                                                 calculate_statistics, [img], [tf.float32]))
+
+
+
+    vol_ = [i[1] for i in list(lung_vol_pixel.take(1).as_numpy_iterator())]
+    lung_stats = list(lung_img_statistics.take(1).as_numpy_iterator())
+    print(vol_)
+    print(lung_stats)
---
+++ osic_utils.py
@@ -0,0 +1,219 @@
+import numpy as np
+import pandas as pd
+import os
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+
+def preprocess_structured_data(data, pid_dict, encoder_dicts, return_df = True):
+    """
+        A function to preprocess the structured data.
+        data = a pandas dataframe with the structured data
+        pid dict = dictionary of patients with id num
+        scaler_dicts = a dictionary of encoders for standardizing and encoding
+    """
+    required_enc = np.array(['SmokingStatus', 'Age', 'Weeks', 'pct_bsl', 'baseline'])
+    bools = np.array([i in encoder_dicts.keys() for i in required_enc])
+    missing_enc = np.where(bools == False)[0]
+
+    assert missing_enc.shape[0] == 0, \
+    "Please specify " + ', '.join(list(required_enc[missing_enc])) + ' encoders'
+
+    ### subset the data into the required pids, aggregate duplicate data
+    data = data.groupby(
+        list(data.columns[~data.columns.isin(["FVC", "Percent"])])
+    ).agg({'FVC' : 'mean',
+       'Percent' :'mean'}).reset_index()
+    data['id_no'] = data.merge(
+        pd.Series(pid_dict).to_frame() \
+        .reset_index() \
+        .rename(columns = {'index': 'Patient', 0: 'id_no'}),
+        on = "Patient"
+    )['id_no']
+
+    ### One hot encoding categorical variables ####
+    # smoking status
+    oh_encoder = encoder_dicts['SmokingStatus']
+    smoking_one_hot_encode = list(oh_encoder.categories_[0])
+    data[smoking_one_hot_encode] = pd.DataFrame(oh_encoder.transform(
+        data['SmokingStatus'].values.reshape(-1, 1)))
+
+    # remove a level from smoking status to avoid non-full rank matrix
+    data.drop(columns = ['Currently smokes'], inplace = True)
+
+    # gender
+    data['Sex_'] = np.where(data['Sex'] == 'Male', 1, 0)
+
+    # drop other columns
+    data.drop(columns = ['Sex', 'SmokingStatus'], inplace = True)
+
+    # Standardize Age
+    age_scaler = encoder_dicts['Age']
+    data['Age'] = pd.DataFrame(
+        age_scaler.transform(
+    data['Age'].values.reshape(-1,1)))
+
+    data.sort_values(['Patient', 'Weeks'], inplace = True)
+
+    # Normalize weeks by their actual start weeks
+    data['Weeks'] = data['Weeks'] - data.groupby(['Patient'])['Weeks'].transform(min)
+
+    # esatablish baseline fvc and percentages for each patient
+    data['baseline'] = pd.merge(
+        data[['Patient']],
+        data.loc[data['Weeks'] == 0][['Patient', 'FVC']],
+        on = "Patient", how = 'left')['FVC']
+
+    data['pct_bsl'] = pd.merge(
+        data[['Patient']],
+        data.loc[data['Weeks'] == 0][['Patient', 'Percent']],
+        on = "Patient", how = "left")['Percent']
+
+    # Standardize weeks, baseline fvc, and percentage baseline
+    for col in ['Weeks', 'baseline', 'pct_bsl']:
+        colname = col + '_std'
+        data[colname] = encoder_dicts[col] \
+        .transform(
+            data[col].values.reshape(-1,1))
+
+    #return data.drop(columns = ['Patient', 'Weeks'])
+
+    X = data.drop(columns = ['Patient', 'Weeks', 'FVC', 'Percent',
+                             'baseline', 'pct_bsl', 'Percent', 'id_no']
+                 )[["Weeks_std", "Age", "Ex-smoker",
+                    "Never smoked", "Sex_", "baseline_std", "pct_bsl_std"]]
+    print('columns in X:', ', '.join(X.columns))
+    #X_tmp = data['Weeks_std'].values
+    ids = data['id_no'].values
+    y =  data['FVC'].values
+
+    if return_df:
+        return data
+
+    return X.values, ids, y
+
+
+class DatasetGen:
+    def __init__(self, pid_dict, split, seed, root_dir, batch_size = 32):
+        """pid = list of Patient IDs
+           split = Train %
+           seed = random seed
+        """
+        np.random.seed(seed)
+        n = len(pid_dict)
+        keys = list(pid_dict.keys())
+        trid = np.random.choice(keys, int(n * split), replace = False)
+        tsid = np.setdiff1d(keys, trid)
+
+
+        self.train = tf.data.Dataset.from_generator(
+            lambda: [(root_dir + k + "/" ,pid_dict[k]) for k in trid],
+        (tf.string, tf.int32)).shuffle(200).batch(batch_size)
+        self.test = tf.data.Dataset.from_generator(
+            lambda: [(k, pid_dict[k]) for k in tsid],
+        (tf.string, tf.int32))
+
+    def add_struct_data(self, struct_data, id_tensor, y):
+        """Adds structured data"""
+
+        def add(id_str, id_, struct_data, id_tensor, y):
+            idx = [tf.squeeze(tf.where(id_tensor == i)) for i in id_]
+            feat = tf.concat([tf.gather(struct_data, i, axis = 0) for i in idx], axis = 0)
+            label = tf.concat([tf.gather(y, i, axis = 0) for i in idx], axis = 0)
+            idx = tf.concat([tf.ones_like(j) * i for i, j in enumerate(idx)], axis = 0)
+
+            return id_str, id_, feat, label, idx
+
+        ds = self.train.map(lambda id_str, x:
+                            tf.py_function(add, [id_str, x, struct_data, id_tensor, y],
+                                           Tout = [tf.string, tf.int32, tf.float64, tf.float64, tf.int64]))
+        ds_test = self.test.map(lambda id_str, x:
+                                tf.py_function(add, [id_str, x, struct_data, id_tensor, y],
+                                                Tout = [tf.string, tf.int32, tf.float64, tf.float64, tf.int64]))
+
+        self.train = ds
+        self.test = ds_test
+
+    def add_img_data(self):
+
+        def add(id_str, idx):
+            imgs = [ct.image_preprocess(i) for i in id_str]
+
+            # calculating lung volume
+            lung_vol_out = []
+            for masked_img, st, ps, lps in imgs:
+                lung_vol_out.append(
+                ct.get_volume(masked_img, tf.constant(st), tf.constant(ps), lps)
+                )
+
+            # calculating lung image statistics
+            stats = []
+            for img, _ in lung_vol_out:
+                stats.append(
+                    ct.calculate_statistics(tf.constant(img))[None, ...])
+            stats = tf.concat(stats, axis = 0)
+
+            ### return lung volume, mean, variance, skew and kurtosis in that order
+            # divide by the magnitude to decrease range
+            lung_volume = tf.concat([i[1][None, ...] for i in lung_vol_out], axis = 0) /1e5
+            all_feats = tf.cast(
+                tf.concat([lung_volume, stats], axis = 1),
+                dtype = tf.float32)
+            all_feats = tf.gather(all_feats, idx, axis = 0)
+
+            return all_feats
+
+        ds = self.train.map(lambda id_str, _, __, lbl, idx:
+                               (_, __, lbl, idx, tf.py_function(add, [id_str, idx], tf.float32))
+                           )
+        self.train = ds
+
+
+
+
+if __name__ == "__main__":
+    train_data = pd.read_csv("/kaggle/input/osic-pulmonary-fibrosis-progression/train.csv")
+    test_data = pd.read_csv("/kaggle/input/osic-pulmonary-fibrosis-progression/test.csv")
+
+    patient_dcm_dict = {}
+    root_dir = "/kaggle/input/osic-pulmonary-fibrosis-progression/train/"
+    for dirname, _, filenames in os.walk(root_dir):
+        if 'ID' in dirname:
+            dirname_ = dirname.replace(root_dir, "")
+            patient_dcm_dict[dirname_] = filenames
+    patient_dcm_dict = {k: i for i, k in enumerate(sorted(patient_dcm_dict.keys()))}
+
+
+    ### fit encoders before running preprocessing data ###
+    # one-hot encoder
+    oh_encoder = OneHotEncoder(sparse = False, dtype = 'int32')
+    oh_encoder.fit(train_data['SmokingStatus'].values.reshape(-1, 1))
+
+    ### Standard scalers ####
+    # AGE
+    age_scaler = StandardScaler()
+    age_scaler.fit(train_data[['Patient', 'Age']].drop_duplicates()['Age'] \
+                   .values.reshape(-1, 1))
+    # WEEKS
+    week_scaler = StandardScaler()
+    week_scaler.fit(train_data[['Patient', 'Weeks']].drop_duplicates()['Weeks'] \
+                   .values.reshape(-1,1))
+    # PERCENTAGE
+    pct_scaler = StandardScaler()
+    pct_scaler.fit(train_data['Percent'].values.reshape(-1,1))
+    # BASELINE FVC
+    baseline_scaler = StandardScaler()
+    baseline_scaler.fit(train_data['FVC'].values.reshape(-1,1))
+
+    encoder_dict = {
+        'Age': age_scaler,
+        'Weeks': week_scaler,
+        'pct_bsl': pct_scaler,
+        'baseline': baseline_scaler,
+        'SmokingStatus': oh_encoder
+    }
+    df_processed = preprocess_structured_data(
+        train_data,
+        patient_dcm_dict,
+        encoder_dict, return_df = True)
+
+    print(df_processed)
